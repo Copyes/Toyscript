ab93957ec3359032aa656ade8dbe511933b25bfe
Copyes <fan_545697167@qq.com>
2018-02-21 00:24:53 +0800

    :sparkles: add dep

--- src/observer/index.js
+++ src/observer/index.js
@@ -1,8 +1,9 @@
 import { arrayMethods } from './array'
-import { def } from './dep'
+import { def, hasOwn, isObject, isPlainObject } from './dep'
 
 const arrayKeys = Object.getOwnPropertyNames(arrayMethods)
-
+// can we use __proto__?
+export const hasProto = '__proto__' in {}
 export class Observer {
   constructor(value) {
     this.value = value
@@ -10,6 +11,8 @@ export class Observer {
     this.vmCount = 0
     def(value, '__ob__', this)
     if (Array.isArray(value)) {
+      const augment = hasProto ? protoAugment : copyAugment
+      augment(value, arrayMethods, arrayKeys)
       this.observeArray(value)
     } else {
       this.walk(value)
@@ -29,5 +32,68 @@ export class Observer {
     }
   }
 }
+/**
+ * 直接覆盖原型的方法来修改目标对象或数组
+ * @param {*} target
+ * @param {*} src
+ */
+function protoAugment(target, src) {
+  target.__proto__ = src
+}
+/**
+ * 定义（覆盖）目标对象或数组的某一个方法
+ * @param {*} target
+ * @param {*} src
+ * @param {*} keys
+ */
+function copyAugment(target, src, keys) {
+  for (let i = 0, l = keys.length; i < l; ++i) {
+    const key = keys[i]
+    def(target, key, src[key])
+  }
+}
+export function observe(value) {
+  if (!isObject(value)) return
+  let ob
+  if (hasOwn(value, '__ob__') && value.__ob__ instanceof Observer) {
+    ob = value.__ob__
+  } else if (
+    (Array.isArray(value) || isPlainObject(value)) &&
+    Object.isExtensible(value) &&
+    !value._isVue
+  ) {
+    ob = new Observer(value)
+  }
 
-export function defineReactive(obj, key, val) {}
+  return ob
+}
+
+export function defineReactive(obj, key, val) {
+  const dep = new Dep()
+  const property = Object.getOwnPropertyDescriptor(obj, key)
+
+  if (property && property.configurable === false) return
+
+  const setter = property && property.set
+  const getter = property && property.get
+
+  let childOb = observe(val)
+  Object.defineProperty(obj, key, {
+    enumerable: true,
+    configurable: true,
+    get: function reactiveGetter() {
+      const value = getter ? getter.call(obj) : val
+      if (Dep.target) {
+        Dep.depend()
+        if (childOb) {
+          childOb.dep.depend()
+          if (Array.isArray(value)) {
+            dependArray(value)
+          }
+        }
+      }
+      return value
+    },
+    set: function reactiveSetter() {}
+  })
+}
--- src/observer/utils.js
+++ src/observer/utils.js
@@ -1,8 +1,31 @@
-export function def(obj, key, enumerable) {
+export function def(obj, key, val, enumerable) {
   Object.defineProperty(obj, key, {
     value: val,
     enumerable: !!enumerable,
     writable: true,
     configurable: true
   })
+}
+
+const hasOwnProperty = Object.prototype.hasOwnProperty
+const _toString = Object.prototype.toString
+
+export function hasOwn(obj, key) {
+  return hasOwnProperty.call(obj, key)
+}
+
+export function isObject(val) {
+  return obj !== null && typeof obj === 'object'
+}
+
+export function isPlainObject(obj) {
+  return _toString.call(obj) === '[object Object]'
+}
+export function remove(arr, item) {
+  if (arr.length) {
+    const index = arr.indexOf(item)
+    if (index > -1) {
+      return arr.splice(index, 1)
+    }
+  }
 }
